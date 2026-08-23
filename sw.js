54be99a9bb89bfaeac9844f0f4790e22f6db3750
CM-ONE <one.cmeg@gmail.com>
2026-08-24 05:13:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="bizodit-v3";
+const CACHE="bizodit-v4";
 const CORE=["./","./index.html","./manifest.json","/assets/logo.png","./auth/signin.html","./css/cm.css","./css/dashboard.css","./css/forms.css","./css/responsive.css","./css/splash.css","./css/loading.css","./css/auth.css","./js/app.js","./js/auth.js","./js/permissions.js","./js/date.js","./js/navigation.js","./js/loading.js","./js/splash.js","./js/export.js","./js/utils.js","./js/firebase-config.js"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
